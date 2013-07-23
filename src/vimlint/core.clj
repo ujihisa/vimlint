786c34d51cf1722e78aeb606d69f52b837f430be
ujihisa <ujihisa@gmail.com>
2013-07-22 21:01:17 -0700

    indentation function

--- src/vimlint/core.clj
+++ src/vimlint/core.clj
@@ -1,7 +1,11 @@
 (ns vimlint.core
   (:require [clojure.string :as s]))
 
+(defn indentation [lines depth]
+  ; TODO: use depth
+  (every? #(re-find #"^$|^[^\t]+" %) lines))
+
 (defn -main [fname]
   (let [lines (s/split-lines (slurp fname))]
     "just check indentation for now"
-    (prn (every? #(re-find #"^$|^[^\t]+" %) lines))))
+    (prn (indentation lines 2))))
