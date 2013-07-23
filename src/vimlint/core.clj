f00d9843cf9db81f7a95ce48121604dc3c0157a1
ujihisa <ujihisa@gmail.com>
2013-07-22 21:22:08 -0700

    parse it

--- src/vimlint/core.clj
+++ src/vimlint/core.clj
@@ -1,11 +1,17 @@
 (ns vimlint.core
   (:require [clojure.string :as s]))
 
+(defn read-list-vim-functions []
+  (for [line (s/split-lines (slurp "resources/functions.dict"))
+        :let [[_ fname args] (re-matches #"(\w+)(.*)" line)]]
+    {:fname fname :args args}))
+
 (defn indentation [lines depth]
   ; TODO: use depth
   (every? #(re-find #"^$|^[^\t]+" %) lines))
 
 (defn -main [fname]
-  (let [lines (s/split-lines (slurp fname))]
+  (prn (read-list-vim-functions))
+  #_(let [lines (s/split-lines (slurp fname))]
     "just check indentation for now"
     (prn (indentation lines 2))))
